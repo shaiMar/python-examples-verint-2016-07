e48cfc74871c218ffd33b92c554e15367dce9253
shaiMar <shai_marko@yahoo.com>
2016-07-05 16:33:39 +0300

    Solution for: sol-python-verint.syntax-lab/01.py

--- 05_syntax_lab/01.py
+++ 05_syntax_lab/01.py
@@ -3,3 +3,14 @@ Write a program that reads 10 numbers from
 the user and prints the largest one
 """
 
+largestNumber = int(0)
+
+for n in range(10):
+    print"please enter number(%d)" %(n+1)
+    currentnumber=int(raw_input())
+    if largestNumber < currentnumber:
+        largestNumber = currentnumber
+
+
+
+print('largest number is :'),largestNumber  
