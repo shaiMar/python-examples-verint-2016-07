9dfff87946538676c8b0c8da073407e797f81efe
shaiMar <shai_marko@yahoo.com>
2016-07-06 13:27:45 +0300

    Solution for: sol-python-verint.syntax-lab/05.py

--- 05_syntax_lab/05.py
+++ 05_syntax_lab/05.py
@@ -3,3 +3,15 @@ Write a program that randomizes integers in a loop
 until it finds a number that is divisable by: 7, 13 and 15
 """
 
+from random import randint
+
+numberToDiviseto = 7*13*15
+
+
+while True:
+    num = randint(1,10000000)
+    if num % numberToDiviseto ==0:
+        print('the number {} is devisable by {}').format(num,numberToDiviseto)
+        break
+    else:
+        print('the number {} isnt devisable by {}').format(num,numberToDiviseto)
