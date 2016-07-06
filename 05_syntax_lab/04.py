c5561a3a80c337150985e47536facda2f0096e19
shaiMar <shai_marko@yahoo.com>
2016-07-06 13:20:21 +0300

    Solution for: sol-python-verint.syntax-lab/04.py

--- 05_syntax_lab/04.py
+++ 05_syntax_lab/04.py
@@ -6,3 +6,21 @@ print all previously inserted lines in reverse
 order (from last to first)
 """
 
+line = "ff"
+
+totalstring = ""
+
+
+while line:
+    print('enter next line:')
+    line = raw_input()
+    totalstring = line + ';' + totalstring
+
+stringsplits = totalstring.split(';')
+
+
+print('here are the inputed lines in revers:')
+for revline in stringsplits:
+    if revline:
+      print revline
+
