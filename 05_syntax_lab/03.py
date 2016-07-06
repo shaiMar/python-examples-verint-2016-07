7f34197fb5a3e44f33490e0f492f45ccb820a68d
shaiMar <shai_marko@yahoo.com>
2016-07-06 10:28:03 +0300

    Solution for: sol-python-verint.syntax-lab/03.py

--- 05_syntax_lab/03.py
+++ 05_syntax_lab/03.py
@@ -5,3 +5,19 @@ For example if the number was: 2345
 The result should be: 14
 """
 
+from random import randint
+
+num = randint(1,1000000)
+
+numberString = str(num)
+
+stringlen = len(numberString)
+
+sum = int(0)
+# go over all the chars in the string and summarize it value 
+for index in range(0,stringlen):
+    sum = sum + int(numberString[index])
+
+
+print "the number is {} and the sum is {}".format(num,sum)
+
