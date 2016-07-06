bfb9a15fa3fdb710da8c21e67783246064bd5700
shaiMar <shai_marko@yahoo.com>
2016-07-06 10:07:05 +0300

    Solution for: sol-python-verint.syntax-lab/02.py

--- 05_syntax_lab/02.py
+++ 05_syntax_lab/02.py
@@ -4,3 +4,15 @@ and prints their sum total.
 If the sum is divisable by 7, also print the word "Boom"
 """
 
+from random import randint
+
+sum = int(0)
+
+for n in range(3):
+    num = randint(1,100)
+    sum = sum + num
+
+print ('the sum of all numbers:'),sum
+
+if sum % 7 == 0:
+    print('Boom!')
