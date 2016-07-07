3c0526caf1d6830cea4c34264fffe0ad5515ff29
shaiMar <shai_marko@yahoo.com>
2016-07-07 10:08:03 +0300

    Solution for: sol-python-verint.syntax-lab/07.py

--- 05_syntax_lab/07.py
+++ 05_syntax_lab/07.py
@@ -6,3 +6,19 @@ After each guess print a hint "too large" or "too small" to the user.
 Bonus: To make things interesting, the program should cheat once in a white
 """
 
+from random import randint
+
+the_number = randint(0,1000)
+
+input_number = 0
+
+while the_number != input_number:
+    print ('enter number:')
+    input_number = int(raw_input())
+    if input_number ==  the_number:
+        print ("BOOOOOOOOOOOOOOOM!")
+        break;
+    if input_number >  the_number:
+        print ("too large!")
+    else:
+        print ("too small!")
