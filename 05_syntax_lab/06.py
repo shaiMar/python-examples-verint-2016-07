ea22b6ae8bafc00643f72898cc11a25b5e883fc1
shaiMar <shai_marko@yahoo.com>
2016-07-07 10:01:12 +0300

    Solution for: sol-python-verint.syntax-lab/06.py

--- 05_syntax_lab/06.py
+++ 05_syntax_lab/06.py
@@ -7,3 +7,14 @@ For example if the numbers were 4 and 6,
 program should print 12
 """
 
+from random import randint
+
+a = randint(1,1000)
+b = randint(1,1000)
+
+for muliplier in range(a+b,a*b+1):
+    if muliplier % a == 0 and muliplier % b ==0:
+        print('the number {} is divisable by {} and {}').format(muliplier,a,b)
+        break;
+
+
