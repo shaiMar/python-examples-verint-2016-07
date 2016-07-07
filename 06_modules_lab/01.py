ce7d3745456f2e2510eaa5220ee0451398b2b30d
shaiMar <shai_marko@yahoo.com>
2016-07-07 10:20:18 +0300

    Solution for: sol-python-verint.modules-lab/01.py

--- 06_modules_lab/01.py
+++ 06_modules_lab/01.py
@@ -2,3 +2,15 @@
 from sys.argv import and prints "Hello Python"
 count times
 """
+
+import sys
+
+times_to_print = 0
+
+if len(sys.argv) > 1:
+    times_to_print = int(sys.argv[1])
+
+
+for n in range(times_to_print):
+     print('Hello Python')
+
