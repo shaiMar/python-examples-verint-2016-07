e11c676c03036a6a21bce01136289038c04dc815
shaiMar <shai_marko@yahoo.com>
2016-07-15 00:24:28 +0300

    Solution for: sol-python-verint.datastructures-lab/01.py

--- 10_datastructures_lab/01.py
+++ 10_datastructures_lab/01.py
@@ -1,26 +1,10 @@
 """
-A file named hosts holds hostnames and IP addresses
-in format: hostname=ip
-Write a program that reads the file and takes
-a list of hostnames in sys.argv
-Program should print the IP addresses of the hosts requested
+Use range() and list comprehension to get
+the list of all lowercase english letters
+Hint: look for chr() and ord()
 """
-import sys
 
-host_ip_map = {}
-
-with  open("d:\hosts",'r') as ff:
-    for line in ff:
-        key_value=str.split(line,'=')
-        key = (key_value[0]).rstrip()
-        value = key_value[1].rstrip() 
-        if key in host_ip_map:
-            host_ip_map[key] =  host_ip_map[key]  + ';' + value
-        else:
-            host_ip_map[key] = value
-
-for k in sys.argv[1:]:
-    if k not in host_ip_map:
-        print 'host '+k+' is empty'
-        continue
-    print ('host:{} ips:{}').format(k,host_ip_map[k])
+oa = ord('a')
+oz = ord('z')
+list = [chr(X) for X in range(256) if X>=oa and X<=oz]
+print list
