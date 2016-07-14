6ad2a32840c117b740881442862b4a349faf12d7
shaiMar <shai_marko@yahoo.com>
2016-07-15 00:33:16 +0300

    Solution for: sol-python-verint.list-comprehension-lab/02.py

--- 12_listcomprehension_lab/02.py
+++ 12_listcomprehension_lab/02.py
@@ -1,6 +1,10 @@
 """
-Write a python program that takes two words
-as sys.argv and prints only the letters
-common to both
+Find and print all possible sequences
+of 3 lowercase characters: aaa, aab, aac, ..., zzz
 """
 
+lower_case_list = range(97,122)
+
+seq = [str(chr(X)+chr(Y)+chr(Z)) for X in lower_case_list for Y in lower_case_list for Z in lower_case_list ] 
+
+print seq
